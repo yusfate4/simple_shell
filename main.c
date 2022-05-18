ebac9d5f334f1580ce9ca8fe09e91c5124306ce9
kennykded <82447341+kennykded@users.noreply.github.com>
2022-05-19 00:46:39 +0100

    Update main.c file

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 void set_data(data_shell *datash, char **av)
 {
-	unsined int i;
+	unsigned int i;
 
 	datash->av = av;
 	datash->input = NULL;
@@ -63,11 +63,11 @@ int main(int ac, char **av)
 	data_shell datash;
 	(void) ac;
 
-	signal(SIGINT, get_sighint);
+	signal(SIGINT, get_sigint);
 	set_data(&datash, av);
 	shell_loop(&datash);
 	free_data(&datash);
 	if (datash.status < 0)
 		return (255);
-	return (data.status);
+	return (datash.status);
 }
